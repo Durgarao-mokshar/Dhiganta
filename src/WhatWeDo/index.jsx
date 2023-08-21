8fb3d63db8916a09f4740e3d36a7708f6b7d8b5f
Durgarao310 <gantadurgarao304@gmail.com>
2023-08-21 18:25:48 +0530

    changes in dhiganta

--- src/WhatWeDo/index.jsx
+++ src/WhatWeDo/index.jsx
@@ -1,41 +1,75 @@
 export default function WhatWeDo(){
-    return <div className="max-w-7xl px-4 gap-14 mx-auto lg:py-36 md:py-28 py-10 ">
-        <div>
-            <h1 className="text-black font-bold lg:text-5xl md:text-3xl text-2xl text-start mb-7">What we do</h1>
-        </div>
-        <div className="grid lg:grid-cols-3 grid-cols-1 gap-10">
-            <div>
-                <h2 className="text-black font-bold mb-10 text-2xl absolute">Storytelling</h2>
-                <span className="relative text-gray-500 text-2xl font-bold  -z-10 top-4 p-4 -left-4 opacity-40">01</span>
-                <ul className="list-disc p-4 mt-4">
-                    <li> captivating narratives </li>
-                    <li>concept to execution</li>
-                    <li>bringing your stories to life </li>
-                    <li>passionate about storytelling</li>
-                </ul>
-            </div>
+    return (
+		<>
+			<div className=" ">
+				<div className=" text-black min-h-screen h-full flex justify-center items-center">
+					<div className="text-center max-w-7xl mx-auto w-full p-2">
+						<div className="text-center">
+							<p className="lg:text-4xl md:text-3xl sm:text-2xl text-xl font-semibold bebas">
+								<span className="">What We Do</span>
+							</p>
+						</div>
+						<div className="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 gap-4 text-gray-900 popins">
+							<div className=" shadow-lg p-6 rounded-md flex gap-6 flex-col hover:bg-green-300 transform transition-all duration-300 hover:scale-95">
+								<div>
+									<img src="./visualization.png" alt="visualization" className="w-1/2 mx-auto" />
+									<p className="lg:text-4xl md:text-3xl text-2xl font-semibold bebas text-center">Visualization</p>
+								</div>
+								<ul className="text-center">
+									<li className="border-b-3 bordr-2 p-1 popins"> Explainer Videos</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> 3D Videos</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> 2D Videos</li>
+									<li className="p-1"> Live Shoot</li>
+								</ul>
+							</div>
+							<div className=" shadow-lg p-6 rounded-md flex gap-6 flex-col hover:bg-green-300 transform transition-all duration-300 hover:scale-95">
+								<div>
+									<img src="./development.png" alt="development" className="w-1/2 mx-auto" />
+									<p className="lg:text-4xl md:text-3xl text-2xl font-semibold text-center bebas">Development</p>
+								</div>
+								<ul className="text-center">
+									<li className="border-b-3 bordr-2 p-1 popins"> Website</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> Mobile</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> UI</li>
+									<li className="p-1"> UX</li>
+								</ul>
+							</div>
+							<div className=" shadow-lg p-6 rounded-md flex gap-6 flex-col hover:bg-green-300 transform transition-all duration-300 hover:scale-95">
+								<div>
+									<img src="./positioning.png" alt="positioning" className="w-1/2 mx-auto" />
+									<p className="lg:text-4xl md:text-3xl text-2xl font-semibold text-center bebas">Positioning</p>
+								</div>
+								<ul className="text-center">
+									<li className="border-b-3 bordr-2 p-1 popins"> Branding</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> Strategy</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> Digital</li>
+									<li className="p-1"> Packaging</li>
+								</ul>
+							</div>
+							<div className=" shadow-lg p-6 rounded-md flex gap-6 flex-col hover:bg-green-300 transform transition-all duration-300 hover:scale-95">
+								<div>
+									<img src="./metaverse.png" alt="metaverse" className="w-1/2 mx-auto" />
+									<p className="lg:text-4xl md:text-3xl text-2xl font-semibold bebas text-center">Metaverse</p>
+								</div>
+								<ul className="text-center">
+									<li className="border-b-3 bordr-2 p-1 popins"> Augmented Reality</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> Virtual Reality</li>
+									<li className="border-b-3 bordr-2 p-1 popins"> Extended Reality</li>
+									<li className="p-1"> Digital Twin</li>
+								</ul>
+							</div>
+						</div>
 
-            <div>
-                <h2 className="text-black font-bold mb-10 text-2xl absolute">Visualizing </h2>
-                <span className="relative text-gray-500 text-2xl font-bold  -z-10 top-4 p-4 -left-4 opacity-40">02</span>
-                <ul className="list-disc p-4 mt-4">
-                    <li>turning your ideas into powerful visuals</li>
-                    <li>fusion of artistic innovation & technical excellence</li>
-                    <li>evoke emotions, and leave a lasting imprint.</li>
-                    <li>share your stories with the world</li>
-                </ul>
-            </div>
-
-            <div>
-                <h2 className="text-black font-bold mb-10 text-2xl absolute">Reveal</h2>
-                <span className="relative text-gray-500 text-2xl font-bold  -z-10 top-4 p-4 -left-4 opacity-40">03</span>
-                <ul className="list-disc p-4 mt-4">
-                    <li>crafting documentaries</li>
-                    <li>thoughtful storytelling</li>
-                    <li>meticulous research </li>
-                    <li>inspire change and foster understanding</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+						<div className="my-20">
+							<div className="my-10 w-full flex justify-center items-center text-blue-900">
+								<a href="https://mokshar.com" target="_blank" rel="noreferrer" className="text-center block  popins">
+									<button className="px-6 py-2 bg-[#33af4e] text-white rounded-full">GET CREATIVE</button>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 } 
--- src/heroComponent/index.jsx
+++ src/heroComponent/index.jsx
@@ -16,70 +16,62 @@ export default function HeroComponent() {
   // }, []);
 
   return (
-    <>
-      <div className="bg-gradient-to-r from-orange-200 to-gray-300 flex items-center lg:justify-start md:justify-start justify-center lg:p-3 md:p-2 p-1">
-        <img
-          src="./DhigantaLogo.png"
-          alt="Dhigantaa logo"
-          className="h-22 w-32"
-        />
-      </div>
-      <div className="flex items-center justify-center w-full min-h-screen bg-gradient-to-r from-orange-200 to-gray-300">
-        <div className="grid lg:grid-cols-2 lg:gap-32 md:grid-cols-2 md:gap-12 gap-6  max-w-7xl mx-auto px-4">
-          <div className="flex flex-col gap-4 items-center justify-center font-playfair">
-            <div className="capitalize font-bold lg:text-5xl md:text-4xl text-2xl text-gray-700">
-              <Typewriter
-                options={{
-                  strings: ["Essence", "in Motion", "Sagas ✨"],
-                  autoStart: true,
-                  loop: true,
-                  delay: 100,
-                }}
-              />
-            </div>
-            <h1 className="text-lg  text-center mt-4 text-purple-900  font-serif">
-              Embark on captivating journeys with Dhiganta as they skillfully
-              craft visual narratives. Revealing the very soul of each story
-              through the artistry of documentary filmmaking
-            </h1>
+		<>
+			<div className="bg-white flex items-center justify-center lg:p-3 md:p-2 p-1">
+				<img src="./DhigantaLogo.png" alt="Dhigantaa logo" className="h-22 w-32" />
+			</div>
 
-            <div className=" transition-transform ease-in-out duration-500 hover:border-gray-100 hover:scale-110 text-black">
-              <div className="group  relative cursor-pointer border border-gray-700  px-3 py-1  rounded-full transition ease-in-out duration-500 hover:border-gray-100 hover:scale-110">
-                <div className="uppercase text-xl flex items-center relative font-mono text-slate-800">
-                  {"Get Started".split("").map((letter, i) => (
-                    <div
-                      key={i}
-                      className="origin-top transition-transform duration-300 ease-in-out group-hover:scale-y-0"
-                      style={{ transitionDelay: `${i * 80}ms` }}
-                    >
-                      {letter}
-                    </div>
-                  ))}
-                  <div className="uppercase text-xl absolute bottom-0 left-0 flex items-center ">
-                    {"Get Started".split("").map((letter, i) => (
-                      <div
-                        key={i}
-                        className="origin-bottom transition-transform duration-300 ease-in-out scale-y-0 group-hover:scale-y-100"
-                        style={{ transitionDelay: `${i * 80}ms` }}
-                      >
-                        {letter}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <section className="lg:self-center bg-black rounded-md">
-            <img
-              src="Hero.jpg"
-              className="rounded-md w-full"
-              alt="Hero Section"
-              loading="lazy"
-            />
-          </section>
-        </div>
-      </div>
-    </>
-  );
+			<div className="flex items-center justify-center w-full min-h-screen relative">
+				<div className="absolute  min-h-screen heroBgImage inset-0 w-full"></div>
+				<div className="">
+					<div className="flex flex-col gap-4 items-center justify-center font-playfair">
+						<div className="capitalize font-bold lg:text-5xl md:text-4xl text-2xl text-gray-900 ">
+							<Typewriter
+								options={{
+									strings: ['Essence', 'in Motion', 'Sagas ✨'],
+									autoStart: true,
+									loop: true,
+									delay: 100
+								}}
+							/>
+						</div>
+						<h1 className=" text-xl  text-center mt-4 text-blue-900   font-serif max-w-3xl font-bold">
+							Embark on captivating journeys with Dhiganta as they skillfully craft visual narratives. Revealing the very soul of each story
+							through the artistry of documentary filmmaking
+						</h1>
+
+						<div className=" transition-transform ease-in-out duration-500 hover:border-gray-100 hover:scale-110 text-black">
+							<div className="group  relative cursor-pointer border border-gray-900  px-3 py-1  rounded-full transition ease-in-out duration-500 hover:border-gray-800 hover:scale-110">
+								<div className="uppercase text-xl flex items-center relative font-mono text-slate-900">
+									{'Get Started'.split('').map((letter, i) => (
+										<div
+											key={i}
+											className="origin-top transition-transform duration-300 ease-in-out group-hover:scale-y-0"
+											style={{ transitionDelay: `${i * 80}ms` }}
+										>
+											{letter}
+										</div>
+									))}
+									<div className="uppercase text-xl absolute bottom-0 left-0 flex items-center ">
+										{'Get Started'.split('').map((letter, i) => (
+											<div
+												key={i}
+												className="origin-bottom transition-transform duration-300 ease-in-out scale-y-0 group-hover:scale-y-100"
+												style={{ transitionDelay: `${i * 80}ms` }}
+											>
+												{letter}
+											</div>
+										))}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* <section className="lg:self-center bg-black rounded-md">
+						<img src="Hero.jpg" className="rounded-md w-full" alt="Hero Section" loading="lazy" />
+					</section> */}
+				</div>
+			</div>
+		</>
+  )
 }
