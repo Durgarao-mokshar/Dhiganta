90177a6acb0377b0a5f57113928720e12d37b300
vijay0221 <vijaymaroju2003@gmail.com>
2023-08-26 16:28:10 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,24 @@ import HeroComponent from "./heroComponent";
 import FooterComponent from "./FooterComponent";
 import ContactForm from "./ContactForm/ContactForm";
 import Question from "./FaqComponent/FAQ";
-import WhoAreWe from './WhoAreWe/WhoAreWe'
+import WhoAreWe from "./WhoAreWe/WhoAreWe";
 import "./App.css";
 
 export default function App() {
   return (
-		<div>
-			<HeroComponent />
-			<Dhiganta />
-			<WhoAreWe />
-			<WhatWeDo />
-			<AnimationGreen />
-			<Clients />
-			<AnimationImage />
-			<Awards />
-			<Question />
-			<HowCanWeHelp />
-			<ContactForm />
-			<FooterComponent />
-		</div>
-  )
+    <div>
+      <HeroComponent />
+      <Dhiganta />
+      <WhoAreWe />
+      <WhatWeDo />
+      {/* <AnimationGreen /> */}
+      {/* <Clients /> */}
+      {/* <AnimationImage /> */}
+      {/* <Awards /> */}
+      <Question />
+      <HowCanWeHelp />
+      <ContactForm />
+      <FooterComponent />
+    </div>
+  );
 }
